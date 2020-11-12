60b4321fcf096411655c466ccf53a39386d7db67
M-A-Justice <m_a_justice@yahoo.com>
2020-11-11 19:32:12 -0800

    complete react boilerplate

---
+++ server/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}!`));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
